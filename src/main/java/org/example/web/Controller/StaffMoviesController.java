3b94b2a11eeb20cf5b0deb3bef825af4ffb950a7
dianaa-clim <clim.diana2017@gmail.com>
2026-01-10 02:18:39 +0200

    employee - add movie

--- src/main/java/org/example/web/Controller/StaffMoviesController.java
+++ src/main/java/org/example/web/Controller/StaffMoviesController.java
@@ -1,12 +1,10 @@
 package org.example.web.Controller;
 
-import common.Account;
 import common.Movie;
-import jakarta.servlet.http.HttpSession;
 import org.example.server.service.MovieService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/staff/movies")
@@ -18,38 +16,21 @@ public class StaffMoviesController {
         this.movieService = movieService;
     }
 
-    // LISTA FILME
+    // ← ASTA LIPSEA (manage films)
     @GetMapping
-    public String manageMovies(HttpSession session, Model model) {
-        Account user = (Account) session.getAttribute("user");
-        if (user == null || !"Employee".equalsIgnoreCase(user.getRole())) {
-            return "redirect:/login";
-        }
-
+    public String staffMovies(Model model) {
         model.addAttribute("movies", movieService.findAll());
         return "staff/movies";
     }
 
-    // PAGINA ADD MOVIE
-    @GetMapping("/add")
-    public String addMoviePage(HttpSession session, Model model) {
-        Account user = (Account) session.getAttribute("user");
-        if (user == null || !"Employee".equalsIgnoreCase(user.getRole())) {
-            return "redirect:/login";
-        }
-
+    @GetMapping("/new")
+    public String newMovieForm(Model model) {
         model.addAttribute("movie", new Movie());
-        return "staff/add-movie";
+        return "staff/movie_new";
     }
 
-    // SAVE MOVIE
-    @PostMapping("/add")
-    public String saveMovie(@ModelAttribute Movie movie, HttpSession session) {
-        Account user = (Account) session.getAttribute("user");
-        if (user == null || !"Employee".equalsIgnoreCase(user.getRole())) {
-            return "redirect:/login";
-        }
-
+    @PostMapping("/new")
+    public String saveMovie(@ModelAttribute Movie movie) {
         movieService.addMovie(movie);
         return "redirect:/staff/movies";
     }
